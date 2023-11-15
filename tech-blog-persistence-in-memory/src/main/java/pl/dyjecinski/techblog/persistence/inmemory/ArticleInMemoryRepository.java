f2af1236a13b45043597ee1e220bc7b9a4c25af3
Adrian Dyjeciński <adriandyjecinski@gmail.com>
2023-11-15 23:59:06 +0100

    Home page update.

--- tech-blog-persistence-in-memory/src/main/java/pl/dyjecinski/techblog/persistence/inmemory/ArticleInMemoryRepository.java
+++ tech-blog-persistence-in-memory/src/main/java/pl/dyjecinski/techblog/persistence/inmemory/ArticleInMemoryRepository.java
@@ -12,11 +12,11 @@ public class ArticleInMemoryRepository {
     private static final List<Article> articleDatabase = new ArrayList<>();
 
     static {
-        articleDatabase.add(new Article(1, "Co nowego w Java 15?", "Java 15 swoją premierę miała 15 września 2020r. i wprowadziła szereg usprawnień do języka oraz całej platformy."));
-        articleDatabase.add(new Article(2, "Co nowego w Java 14?", "Java 14 została oficjalnie udostępniona 17 marca 2020 roku i przynosi szereg usprawnień do języka.      Niektóre ze zmian oznaczone są jako Preview Feature."));
-        articleDatabase.add(new Article(3, "Co nowego w Java 13?", "Premiera Javy 13 zaplanowana jest na 17 września 2019 roku. Oprócz drobnych usprawnień planowane jest wprowadzenie 5 nowych funkcjonalności"));
-        articleDatabase.add(new Article(4, "Co nowego w Java 12?", "Kolejna wersja Javy: Java 12, oficjalnie pod nazwą JDK 12 osiągnęła poziom w którym zamknięto zakres wprowadzanych funkcjonalności."));
-        articleDatabase.add(new Article(5, "Co nowego w Java 11?", "Wraz z nowym cyklem wydawniczym Javy minęły zaledwie 3 miesiące od wydania Javy 10, a już doczekaliśmy się Javy 11."));
+        articleDatabase.add(new Article(1, "Co nowego w Java 15?", "Java 15 swoją premierę miała 15 września 2020r. i wprowadziła szereg usprawnień do języka oraz całej platformy. Java 15 swoją premierę miała 15 września 2020r. i wprowadziła szereg usprawnień do języka oraz całej platformy."));
+        articleDatabase.add(new Article(2, "Co nowego w Java 14?", "Java 14 została oficjalnie udostępniona 17 marca 2020 roku i przynosi szereg usprawnień do języka.      Niektóre ze zmian oznaczone są jako Preview Feature. Java 15 swoją premierę miała 15 września 2020r. i wprowadziła szereg usprawnień do języka oraz całej platformy."));
+        articleDatabase.add(new Article(3, "Co nowego w Java 13?", "Premiera Javy 13 zaplanowana jest na 17 września 2019 roku. Oprócz drobnych usprawnień planowane jest wprowadzenie 5 nowych funkcjonalności. Java 15 swoją premierę miała 15 września 2020r. i wprowadziła szereg usprawnień do języka oraz całej platformy."));
+        articleDatabase.add(new Article(4, "Co nowego w Java 12?", "Kolejna wersja Javy: Java 12, oficjalnie pod nazwą JDK 12 osiągnęła poziom w którym zamknięto zakres wprowadzanych funkcjonalności. Java 15 swoją premierę miała 15 września 2020r. i wprowadziła szereg usprawnień do języka oraz całej platformy."));
+        articleDatabase.add(new Article(5, "Co nowego w Java 11?", "Wraz z nowym cyklem wydawniczym Javy minęły zaledwie 3 miesiące od wydania Javy 10, a już doczekaliśmy się Javy 11. Java 15 swoją premierę miała 15 września 2020r. i wprowadziła szereg usprawnień do języka oraz całej platformy."));
     }
 
     public void add(Article article) {
